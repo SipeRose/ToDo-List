771fe9545c997f204e1c79a9ff87579b9f213c32
Nikita Volkov <nikita.volkov01@mail.ru>
2025-01-24 15:26:45 +0300

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Никита Волков on 24.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
